1334035c9e2afe7ee778a47520e53d82c183ab19
Your Name <you@example.com>
2021-11-02 11:12:17 -0700

    updates

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var message = 'Hello Node!';
 
-var sum = 5 + 3;
 
-console.log(message);
-console.log(sum);
+//var profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Miles',
+  age: 99
+};
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+process.argv[1]
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+console.log(err)
+
+//.slice () example 
+//const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+//console.log(animals.slice(2, 6));
