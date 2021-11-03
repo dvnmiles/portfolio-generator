8ff9b80cb49000192a645d548b102084ce39ba67
Your Name <you@example.com>
2021-11-03 13:34:26 -0700

    update

--- app.js
+++ app.js
@@ -1,56 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const nameD = profileDataArgs[0];
-const github = profileDataArgs[1];
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const profileDataArgs = process.argv.slice(2);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
+console.log(profileDataArgs);
 
-console.log(generatePage(nameD, github));
+const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const printProfileData = profileDataArr => {
-  // This...
-  //for (let i = 0; i < profileDataArr.length; i += 1) {
-    //console.log(profileDataArr[i]);
-  //}
-
- // console.log('================');
-
-  // Is the same as this...
- // profileDataArr.forEach(profileItem => console.log(profileItem));
-//};
-
-//printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
